d3752804d003c7116498ed4728e4e93dce9636c5
Webismagic <96910106+Webismagic@users.noreply.github.com>
2024-02-23 20:23:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import plotly.express as px
+import streamlit as st
+from model import Graphic
+
+gf = Graphic(2023)
+
+fig = gf.get_graphic_type_1(2023)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
 
