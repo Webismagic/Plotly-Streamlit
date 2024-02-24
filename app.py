43c7d1e4c3f33bda3bdae2ca46df4e32cf9214e2
Webismagic <96910106+Webismagic@users.noreply.github.com>
2024-02-24 18:20:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ fig4 = gf.get_graphic_type_4(2023)
 fig5 = gf.get_graphic_type_5(2023)
 fig6 = gf.get_graphic_type_6(2023)
 fig7 = gf.get_graphic_type_7(2023)
+fig8 = gf.get_graphic_type_8(2023)
+fig9 = gf.get_graphic_type_9(2023)
+fig10 = gf.get_graphic_type_10(2023)
 fig12 = gf.get_graphic_type_12b(2023)
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab12 = st.tabs(["Chart 1", "Chart 2", "Chart 3", "Chart 4", "Chart 5", "Chart 6", "Chart 7", "Chart 12"])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab12 = st.tabs(["Chart 1", "Chart 2", "Chart 3",
+                                                                              "Chart 4", "Chart 5", "Chart 6",
+                                                                              "Chart 7", "Chart 8", "Chart 9",
+                                                                              "Chart 10", "Chart 12"])
 with tab1:
     # 1.
     # This is the default. So you can also omit the theme argument.
@@ -35,6 +41,15 @@ with tab6:
 with tab7:
     # 7.
     st.plotly_chart(fig7, theme="streamlit", use_container_width=True)
+with tab8:
+    # 8.
+    st.plotly_chart(fig8, theme="streamlit", use_container_width=True)
+with tab9:
+    # 9.
+    st.plotly_chart(fig9, theme="streamlit", use_container_width=True)
+with tab10:
+    # 10.
+    st.plotly_chart(fig10, theme="streamlit", use_container_width=True)
 with tab12:
     # 12.
     st.plotly_chart(fig12, theme="streamlit", use_container_width=True)
