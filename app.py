b11e0485a12366757ce5a202d576593b62e8a3f2
Webismagic <96910106+Webismagic@users.noreply.github.com>
2024-02-24 19:37:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import plotly.express as px
 import streamlit as st
 from model import Graphic as gph
-
-gf = gph.Graphics(2023)
+year = 2023
+gf = gph.Graphics(year)
 fig1 = gf.get_graphic_type_1(year)
-fig2 = gf.get_graphic_type_2(2023)
-fig3 = gf.get_graphic_type_3(2023)
-fig4 = gf.get_graphic_type_4(2023)
-fig5 = gf.get_graphic_type_5(2023)
-fig6 = gf.get_graphic_type_6(2023)
-fig7 = gf.get_graphic_type_7(2023)
+fig2 = gf.get_graphic_type_2(year)
+fig3 = gf.get_graphic_type_3(year)
+fig4 = gf.get_graphic_type_4(year)
+fig5 = gf.get_graphic_type_5(year)
+fig6 = gf.get_graphic_type_6(year)
+fig7 = gf.get_graphic_type_7(year)
 fig8 = gf.get_graphic_type_8()
 fig9 = gf.get_graphic_type_9()
 fig10 = gf.get_graphic_type_10()
 fig11 = gf.get_graphic_type_11()
-fig12 = gf.get_graphic_type_12b(2023)
-fig13 = gf.get_graphic_type_13(2023)
+fig12 = gf.get_graphic_type_12b(year)
+fig13 = gf.get_graphic_type_13(year)
 
 tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12, tab13 = st.tabs(["Chart 1", "Chart 2", "Chart 3",
                                                                               "Chart 4", "Chart 5", "Chart 6",
