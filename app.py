29e93f19e55efa7b8513c6566e3c255f2a91cb40
Webismagic <96910106+Webismagic@users.noreply.github.com>
2024-02-24 17:55:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from model import Graphic as gph
 
 gf = gph.Graphics(2023)
 
-fig = gf.get_graphic_type_3(2023)
+fig = gf.get_graphic_type_4(2023)
 
 tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
 with tab1:
