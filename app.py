5cd923876bac7cd90af52682e73794d3458d785f
Webismagic <96910106+Webismagic@users.noreply.github.com>
2024-02-24 18:07:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ fig1 = gf.get_graphic_type_1(2023)
 fig2 = gf.get_graphic_type_2(2023)
 fig3 = gf.get_graphic_type_3(2023)
 fig4 = gf.get_graphic_type_4(2023)
+fig5 = gf.get_graphic_type_5(2023)
 fig12 = gf.get_graphic_type_12b(2023)
 
-tab1, tab2, tab3, tab4, tab12 = st.tabs(["Chart 1", "Chart 2", "Chart 3", "Chart 4", "Chart 12"])
+tab1, tab2, tab3, tab4, tab5, tab12 = st.tabs(["Chart 1", "Chart 2", "Chart 3", "Chart 4", "Chart 5", "Chart 12"])
 with tab1:
     # 1.
     # This is the default. So you can also omit the theme argument.
@@ -23,6 +24,9 @@ with tab3:
 with tab4:
     # 4.
     st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
+with tab:
+    # 5.
+    st.plotly_chart(fig5, theme="streamlit", use_container_width=True)
 with tab12:
     # 12.
     st.plotly_chart(fig12, theme="streamlit", use_container_width=True)
